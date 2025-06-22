a045346dd51752730b8112d4f9107820dc270ee0
Rahamim <raha1996@gmail.com>
2025-06-22 19:26:12 +0300

    fixed  cicd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Flights from './pages/Flights';
 import Booking from './pages/Booking';
-import Homepage from './pages/Homepage'
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/index" element = {<Home />} />
         <Route path="/flights" element={<Flights />} />
         <Route path="/booking/:flight_number" element={<Booking />} />
