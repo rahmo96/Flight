b18752580c979e6349a2dc89a0fa310976ec9966
Rahamim Tadela <raha1996@gmail.com>
2025-11-09 22:24:26 +0200

    page fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Flight',
+  base: '/Flight/frontend',
   test: {
     globals: true,
     environment: 'jsdom',
